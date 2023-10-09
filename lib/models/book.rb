cc1e9d6382a92bec2e4f79c1aebc485345c50ef6
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-10-09 14:29:56 -0400

    Fix linters errors

--- lib/models/book.rb
+++ lib/models/book.rb
@@ -2,13 +2,13 @@ lib/models/book.rb
 
 class Book
   attr_accessor :title, :author, :rentals
-  
+
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
   end
-  
+
   def add_rental(person, date)
     Rental.new(date, self, person)
   end
