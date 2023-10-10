090b4841ad085bf0d3e8878968f061cc990f82a5
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-10 16:26:53 -0500

    Update classroom_spec.rb
    Adding code for classroom class test

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require_relative '../lib/models/classroom'
+
+describe Classroom do
+  subject(:classroom) { Classroom.new("Math 101") }
+
+  describe '#initialize' do
+    it 'initializes with a given label' do
+      expect(classroom.label).to eq("Math 101")
+    end
+  end
+
+  describe '#add_student' do
+    it 'adds a student to the classroom' do
+      student = instance_double("Student")
+      allow(student).to receive(:classroom=)
+      classroom.add_student(student)
+      expect(classroom.students).to include(student)
+    end
+  end
+end
